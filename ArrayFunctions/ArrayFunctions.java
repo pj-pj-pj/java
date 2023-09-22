409ecb6d3734cafe541620bdd3ea2d0518525d54
pj-01 <96671928+pj-pj-pj@users.noreply.github.com>
2023-09-22 10:45:59 +0800

    add array operations: insertion, and update; also refactored the code to use String[] instead to use null and allows more flexibility

--- ArrayFunctions/ArrayFunctions.java
+++ ArrayFunctions/ArrayFunctions.java
@@ -2,52 +2,80 @@ package ArrayFunctions;
 
 public class ArrayFunctions {
   public static void main(String[] args) {
-    int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-    System.out.println();
-
-    System.out.println(retrieve(5, numbers));
-    System.out.println(delete(5, numbers));
-    System.out.println(update(3, 100, numbers));
-
-    System.out.println("\n");
-
-    for (int i = 0; i < numbers.length - 1; i++) {
-      System.out.print(numbers[i] + ",");
-    }
-
-    System.out.println("\n");
   }
 
-  static String retrieve(int num, int[] array) {
-    for (int j = 0; j < array.length - 1; j++) {
-      if (num == array[j]) {
-        return "Element " + num + " successfully found at index [" + j + "].";
+  static String retrieve(String element, String[] array) {
+    // traverse the array until it founds a match to the given element
+    for (int j = 0; j < array.length; j++) {
+      if (element.equals(array[j])) {
+        return "Element " + element + " successfully found at index [" + j + "].";
       }
     }
 
-    return "Element " + num + " not found.";
+    // if the traversal didn't find any element until it reached the end
+    return "Element " + element + " not found, retrieval failed.";
   }
 
-  static String delete(int index, int[] array) {
+  static String delete(int index, String[] array) {
     if (index < 0 || index >= array.length) {
-      return "Invalid index value.";
+      return "Invalid index value, deletion failed.";
     }
 
+    // shift elements from right of index to be deleted
     for (int i = index; i < array.length - 1; i++) {
       array[i] = array[i + 1];
     }
 
-    return "Element at index [" + index + "] sucessfully deleted.";
+    // replace the last element with null
+    // because last and second to the last now has the same value
+    update(array.length - 1, null, array);
+
+    return "Element at index [" + index + "] successfully deleted.";
   }
 
-  static String update(int index, int replacement, int[] array) {
+  static String update(int index, String replacement, String[] array) {
     if (index < 0 || index >= array.length) {
-      return "Invalid index value";
+      return "Invalid index value, updating failed.";
     }
 
     array[index] = replacement;
 
     return "Element at index [" + index + "] successsfully updated.";
+  }
+
+  static String[] insert(int index, String newElement, String[] array) {
+    if (index < 0 || index >= array.length) {
+      return array;
+    }
+
+    // if array has no available slot
+    if (array[array.length - 1] != null) {
+      String[] newArr = new String[array.length + 1];
+
+      // copy original array to array with longer length
+      for (int i = 0; i < index; i++) {
+        newArr[i] = array[i];
+      }
+
+      // shift elements towards
+      // the right of where the new element will be inserted
+      for (int i = index + 1; i < newArr.length; i++) {
+        newArr[i] = array[i - 1];
+      }
+
+      // assign new element to the given index
+      newArr[index] = newElement;
+
+      return newArr;
+    } else {
+      for (int i = array.length - 1; i > index; i--) {
+        array[i] = array[i - 1];
+      }
+
+      // assign the new element to the given index
+      array[index] = newElement;
+
+      return array;
+    }
   }
 }
